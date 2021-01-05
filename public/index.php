40ca92f8c55b2c484b06da37da540fb09c5ca209
Ricardo Fusco <ricardo.fusco2@gmail.com>
2021-01-05 02:43:46 +0000

    Fixed index.php with latest router configs changes

--- public/index.php
+++ public/index.php
@@ -7,7 +7,7 @@ require PROJECT_ROOT . '/vendor/autoload.php';
 use DI\Container;
 use PoolTournament\Application\Module\Core\Entrypoint\Http\Rest\Request;
 use PoolTournament\Application\Module\Core\Entrypoint\Http\Rest\Routing\Router;
-use PoolTournament\Application\Module\Core\Entrypoint\Yaml\Parser as YamlParser;
+use PoolTournament\Application\Module\Core\Entrypoint\Http\Rest\Routing\RouterConfig;
 
 header("Content-Type: application/json");
 
@@ -15,12 +15,11 @@ try {
     /** @var Container $container */
     $container = require PROJECT_ROOT . '/config/dependency-injection.php';
 
-    $yamlParser = new YamlParser(PROJECT_ROOT . '/config/routing.yml');
-    $routerConfigs = $yamlParser->parse();
+    $routerConfigs = RouterConfig::createFromFile(PROJECT_ROOT . '/config/routing.yml');
 
     $request = Request::createFromGlobals();
 
-    $router = Router::createFromConfig($routerConfigs);
+    $router = new Router($routerConfigs);
     $route = $router->matchRequestToRoute($request);
 
     $container->call([$route->getController(), $route->getAction()], [$request]);
